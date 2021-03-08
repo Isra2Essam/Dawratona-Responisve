b966939b6fd4553b05536f6709bbe64c9891fa28
Isra2Essam <israaessam12300@gmail.com>
2021-03-08 03:34:34 +0200

    dawratona UI
    html css bootstrap js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.carousel').carousel({
+    touch:true,
+    keyboard:true
+
+  });
+  $(".nam").tooltip();
+  $(function () {$('[data-toggle="popover"]').popover()})
+  });
